1235a2504fb2579b2a05b8cd5186e0c4782e20ae
Hamza405 <hamzash863@gmail.com>
2023-01-02 11:49:40 +0200

    make main section responsive

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,30 +1,37 @@
 import Button from "../Button";
 import BG from "../../assets/bg-intro-desktop.svg";
+import BGMobile from "../../assets/bg-intro-mobile.svg";
 import Mockup from "../../assets/image-mockups.png";
 
 const Main = () => {
   return (
-    <div className="relative overflow-hidden flex justify-between align-center w-full bg-gray-100">
-      <div className="pl-28 pr-24 py-32 w-1/2">
-        <p className="text-5xl font-light">
+    <div className="relative overflow-hidden  flex justify-between align-center w-full bg-gray-100">
+      <div className="md:block flex justify-center items-center flex-col  md:pl-28 md:pr- py-24 md:py-32 w-full md:w-1/2 mt-[60%] md:mt-0">
+        <p className="text-center md:text-start text-4xl md:text-5xl font-light">
           Next generation
           <br /> digital banking
         </p>
-        <p className="py-8 text-lg text-gray-500">
+        <p className="text-center md:text-start px-6 md:mr-24 md:px-0 py-8 text-lg text-gray-500">
           Take your financial life online, Yor easybank account will be a
           one-stop-shop for spending, saving, budgeting, investing and much
           more.
         </p>
         <Button>Request Invite</Button>
       </div>
-      <img
-        className="w-2/3 object-fill absolute top-0 right-0 translate-x-[20%] translate-y-[-25%]"
-        src={BG}
-      />
-      <img
-        className="w-1/2 object-fill absolute top-0 right-0 translate-x-[20%] translate-y-[-15%] z-10"
-        src={Mockup}
-      />
+      <div>
+        <img
+          className="hidden md:block w-3/4 object-fill absolute top-0 right-0 translate-x-[25%] translate-y-[-35%] md:translate-y-[-25%]"
+          src={BG}
+        />
+        <img
+          className="md:hidden w-full object-fill absolute top-0 right-0 translate-y-[-25%]"
+          src={BGMobile}
+        />
+        <img
+          className="w-3/4 md:w-1/2 object-fill absolute top-0 right-0 translate-x-[-15%] md:translate-x-[20%] translate-y-[-30%] md:translate-y-[-15%] z-10"
+          src={Mockup}
+        />
+      </div>
     </div>
   );
 };
