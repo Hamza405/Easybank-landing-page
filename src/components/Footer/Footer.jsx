59a9c2b231d41ebd7958ccb06eade18ed953cacf
Hamza405 <hamzash863@gmail.com>
2023-01-03 11:55:49 +0200

    mobile footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Button from "../Button";
 import Logo from "../../assets/logo-dark.svg";
 import IconYoutube from "../../assets/icon-youtube.svg";
 import IconTwitter from "../../assets/icon-twitter.svg";
@@ -20,7 +21,7 @@ const Footer = () => {
     <footer className="flex justify-center items-center flex-col py-4 px-8 bg-[#2d314d]">
       <div className="py-4">
         <img src={Logo} alt="Easybank" />
-        <div className="flex justify-between items-center py-6">
+        <div className="flex justify-between items-center py-8">
           <img src={IconYoutube} />
           <img src={IconInstagram} />
           <img src={IconFacebook} />
@@ -32,14 +33,18 @@ const Footer = () => {
         {NavList.map((item) => (
           <p
             key={item}
-            className="my-2 capitalize text-center text-white hover:text-green-500"
+            className="my-2 capitalize text-center cursor-pointer text-white hover:text-green-500"
           >
             {item}
           </p>
         ))}
       </div>
-      <div></div>
-      <div></div>
+      <div className="mt-8 flex items-center justify-between flex-col">
+        <Button>Request Invite</Button>
+        <p className="my-8 capitalize text-center text-gray-400">
+          @ Easybank. All Rights Reserved
+        </p>
+      </div>
     </footer>
   );
 };
